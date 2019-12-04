6e173972b9d004daa36ff49d81c0f85006af159c
Jake <gosnelljacob@gmail.com>
2019-12-04 00:27:39 -0800

    demonstrated use of component from react library, using props, passing data from a parent to a child, destructuring the prop from this instance of props and returning a new element with the data of text from the parent element in the child, which thus renders in the parent element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-					<Welcome />
-				</p>
-			</header>
-		</div>
-	);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            <Welcome text="Welcome to Using Props" />
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
-const Welcome = () => {
-  return (
-    <h1 className="App-title">Welcome</h1>
-  );
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    );
+  }
 }
 
 export default App;
