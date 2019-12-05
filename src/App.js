5e7626230de8976bdaded0783464c80fbe7d54e4
Jake <gosnelljacob@gmail.com>
2019-12-05 00:45:53 -0800

    set up basic structure for movie app, created a bare bones movie component that renders a div with the props for each movie title that is being iterated over by the map method over the movies array in the parent App component, thus it returns all the titles of the movies in our movies array data structure with a big O of n (linear run time)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider-Man'
+  },
+  {
+    id: 3,
+    title: '36th Chamber of Shaolin'
+  }
+];
+
 class App extends Component {
 
   state = {
@@ -25,27 +42,10 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<Welcome text="Welcome to Using Props" />
-          <h3>{this.state.input}</h3>
-          <input type="text" onChange={this.updateInput} value={this.state.input} />
-          <input type="email" ref={(input) => this.email = input } />
-					<button onClick={this.submit}>Show Value</button>
 				</header>
+				{movies.map(movie => ( <Movie key={movie.id} movie={movie}/> ))}
 			</div>
 		);
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text, toggle } = this.props;
-    console.log(toggle);
-    return (
-      <h1 className="App-title">{text}</h1>
-    );
   }
 }
 
