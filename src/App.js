eb4bf52cb4e91964b7ba2d709617597a0a6855af
Jake <gosnelljacob@gmail.com>
2019-12-08 15:38:08 -0800

    added real data to application component by using async and await to call it when the component mounts, and displays that data in the Movie component that is rendering as a result of inline javascript iterating over the movies array stored in state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Movie from './Movie';
 
+// dummy data
 const movies = [
   {
     id: 1,
@@ -23,33 +24,51 @@ const movies = [
 ];
 
 class App extends Component {
+  // needs initial state
+	state = {
+		movies: []
+	};
 
-  state = {
-    input: 'Hello'
-  }
+  // real data
+	async componentDidMount() {
+		try {
+			const response = await fetch(
+				"https://api.themoviedb.org/3/discover/movie?api_key=e71760c36ffdf0f10ed5e0db6873ceb8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+			);
+			const movies = await response.json();
+			console.log(movies);
+			this.setState({
+				movies: movies.results
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-  updateInput = (event) => {
-    console.log(event.target.value)
-    this.setState({
-      input: event.target.value.trim() // trim() makes it so a user cannot spacebar in an input
-    })
-  }
+	updateInput = event => {
+		console.log(event.target.value);
+		this.setState({
+			input: event.target.value.trim() // trim() makes it so a user cannot spacebar in an input
+		});
+	};
 
-  submit = () => {
-    console.log(this.state.input)
-  }
+	submit = () => {
+		console.log(this.state.input);
+	};
 
-  //componentMounts()
-  render() {
-    return (
+	//componentMounts()
+	render() {
+		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 				</header>
-				{movies.map(movie => ( <Movie key={movie.id} movie={movie} desc={movie.desc}/> ))}
+				{this.state.movies.map(movie => (
+					<Movie key={movie.id} movie={movie} />
+				))}
 			</div>
 		);
-  }
+	}
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -5,20 +5,14 @@ export default class Movie extends Component {
   static propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
-    }),
-    desc: PropTypes.string
-  }
-
-  static defaultProps = {
-    desc: 'Description not available'
+    })
   }
 
   render() {
-    const { movie, desc } = this.props;
+    const { movie } = this.props;
     return (
       <div>
         <h3>{movie.title}</h3>
-        <p>{desc}</p>
       </div>
     );
   }
